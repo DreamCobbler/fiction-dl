39f57ae9e730dd28644dc536d756ad50368c45ef
Dream Cobbler <dreamcobbler@outlook.com>
2021-06-05 13:17:03 +0200

    Changed the version number to 1.8.3.

--- fiction_dl/Configuration.py
+++ fiction_dl/Configuration.py
@@ -40,7 +40,7 @@ import tempfile
 #
 
 ApplicationName = "fiction-dl"
-ApplicationVersion = "1.8.2"
+ApplicationVersion = "1.8.3"
 ApplicationShortDescription = (
     "A content downloader, capable of retrieving works of (fan)fiction from the web and saving them in a few common file"
     " formats."
